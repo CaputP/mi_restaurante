bba7c8777df236f1f96bfb4ca1b2fdcaa15f624a
climaco <019201596i@uandina.edu.pe>
2026-07-16 08:41:11 -0500

    se edito las rutas de las imagenes en featuresDishes.js

--- src/data/featuredDishes.js
+++ src/data/featuredDishes.js
@@ -1,3 +1,7 @@
+import cuy from "../assets/images/featuredDishes/cuy.png"
+import malaya from "../assets/images/featuredDishes/malaya.png"
+import chicharron from "../assets/images/featuredDishes/chicharron.png"
+import trucha from "../assets/images/featuredDishes/trucha.png"
 
 // ==================================
 // PLATOS DESTACADOS DEL RESTAURANTE
@@ -9,7 +13,7 @@ const featuredDishes = [
         nombre:"Cuy al Horno",
         precio:"s/50",
         descripcion:"Rico cuy acompañado de tallarin al horno con su rocoto relleno y papa",
-        imagen:"../../src/assets/images/featuredDishes/cuy.png",
+        imagen: cuy
     },
     
     {
@@ -17,7 +21,7 @@ const featuredDishes = [
         nombre:"Malaya Frita",
         precio:"s/25",
         descripcion:"Malaya frita con arroz papa frita o sancochada y ensalada",
-        imagen:"../src/assets/images/featuredDishes/malaya.png"
+        imagen: malaya
     },
 
     {
@@ -25,7 +29,7 @@ const featuredDishes = [
         nombre:"Chicharron",
         precio:"s/25",
         descripcion:"Chicharron de cerdo con mote papa y ensalada de hierba buena y cebolla",
-        imagen:"../src/assets/images/featuredDishes/chicharron.png",
+        imagen: chicharron
     },
 
     {
@@ -33,7 +37,7 @@ const featuredDishes = [
         nombre:"Trucha",
         precio:"s/15",
         descripcion:"Trucha Frita con su arroz papa y ensalada",
-        imagen:"../src/assets/images/featuredDishes/trucha.png",
+        imagen: trucha
     },
 ]
 
